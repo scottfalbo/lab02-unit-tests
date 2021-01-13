4fdad6917032b461659b91de4368a9840732a577
Scott Falbo <scottfalboart@gmail.com>
2021-01-12 17:28:51 -0800

    commiting, because I'm lame and haven't yet.

--- FakeATM/Program.cs
+++ FakeATM/Program.cs
@@ -4,9 +4,65 @@ namespace FakeATM
 {
     public class Program
     {
+        public static decimal balance = 5000;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Fake ATM\n");
+            UserInterface();
+        }
+        public static void UserInterface()
+        {
+            Console.WriteLine("Make a selection from the menu:\n1. Check Balance \n2. Deposit \n3. Withdraw \n4. Exit");
+            bool exit = false;
+            while (!exit)
+            {
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine($"balance: {ViewBalance()}");
+                        break;
+                    case "2":
+                        Console.WriteLine("deposit");
+                        Deposit(GetAmount());
+                        break;
+                    case "3":
+                        Console.WriteLine("withdraw");
+                        Withdraw(GetAmount());
+                        break;
+                    case "4":
+                        Console.WriteLine("exit");
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("catch all");
+                        break;
+                }
+                UserInterface();
+            }
+        }
+
+        public static decimal GetAmount()
+        {
+            return 0;
+        }
+        // return current balance
+        public static decimal ViewBalance()
+        {
+            return balance;
+        }
+
+        // withdraw from the balance
+        public static decimal Withdraw(decimal amount)
+        {
+            return decimal.Subtract(balance, amount);
+        }
+
+        // deposit
+        public static decimal Deposit(decimal amount)
+
+        {
+            return decimal.Add(balance, amount);
         }
     }
 }
--- FakeATMTests/UnitTest1.cs
+++ FakeATMTests/UnitTest1.cs
@@ -7,9 +7,26 @@ namespace FakeATMTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CheckBalanceTest()
         {
+            decimal balance = Program.balance;
+            Assert.Equal(Program.balance, balance);
+        }
 
+        [Fact]
+        public void WithdrawTest()
+        {
+            decimal expected = Program.balance - 500;
+            decimal result = Program.Withdraw(500);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void DepositTest()
+        {
+            decimal expected = Program.balance + 500;
+            decimal result = Program.Deposit(500);
+            Assert.Equal(expected, result);
         }
     }
 }
